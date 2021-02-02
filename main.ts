b6053e503b3e44bc2cb76775f6c0bb79be7fbfc0
cakepower <32028127+cakepower@users.noreply.github.com>
2021-02-02 13:19:07 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 RingbitCar.init_wheel(AnalogPin.P7, AnalogPin.P9)
 RingbitCar.freestyle(100, 100)
 basic.forever(function () {
-    RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 90)
+    motorbit.turnright(50)
+    motorbit.turnleft(50)
+    RingbitCar.steering_angle(RingbitCar.Direction_turn.right, -90)
     RingbitCar.brake()
     RingbitCar.steering_angle(RingbitCar.Direction_turn.left, 90)
     RingbitCar.brake()
